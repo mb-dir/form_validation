0fb56acf32eb7cbc4173f318bf68baa7684e8c75
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-15 17:34:06 +0200

    Delete unnecessary content
    This content comes from boilerplate, in this project it is completly
    unnecessary

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,33 +3,3 @@
 import regeneratorRuntime from "regenerator-runtime";
 
 // styles import
-import "../styles/test.scss";
-
-// place your code below(you can delete the code below and write your own)
-
-//js modules import
-import "./test.js";
-
-console.log("Hello 🐼");
-
-//Babel test
-document.body.addEventListener("click", ()=>{
-    console.log("Do not touch me!");
-});
-
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, 2000);
-    });
-  }
-  
-  async function asyncCall() {
-    console.log('calling');
-    const result = await resolveAfter2Seconds();
-    console.log(result);
-    // expected output: "resolved"
-  }
-  
-  asyncCall();
