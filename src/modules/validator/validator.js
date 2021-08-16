4421707edf97e101f705d7955c3c91e78eed35d4
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-16 21:23:43 +0200

    Changes in validator.js structure
    Now the name input is checked. checkUserName function returnes
    true/false, and I can set if conditions based on this

--- src/modules/validator/validator.js
+++ src/modules/validator/validator.js
@@ -1,15 +1,18 @@
 //The only purpose of this module is getting reference to suitable DOM elements(mainly related with form) and, depending on the condition, distribute tasks (checking appropriate form inputs) to other modules
 
+import checkUserName from "./checkUserName";
+
 function formValidator(){
     const form = document.querySelector("#mainForm");
     form.addEventListener("submit", (e)=>{
-        e.preventDefault();
         const formData = new FormData(form);
 
         for(const [key, values] of formData.entries()) {
             if(key === "userName"){
                 //call the function which validates the name
-                console.log("userName")
+                if(checkUserName(values) === false){
+                    e.preventDefault();
+                }
             }
             if(key === "userEmail"){
                 //call the function which validates the email
