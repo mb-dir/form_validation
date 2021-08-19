1854905023a97818fb1186fa11aa56ffc12ee09f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-19 14:37:31 +0200

    Regex update
    The old two regexs was able to combine in one

--- src/modules/validator/checkUserName.js
+++ src/modules/validator/checkUserName.js
@@ -1,8 +1,7 @@
 function checkUserName(name){
-    const specialSignReg = /\W|_/g;//https://stackoverflow.com/questions/18812317/javascript-regex-for-special-characters
-    const numbersReg = /[0-9]/g;
+    const specialSignOrNumbersReg = /[0-9!@#\$%\^\&*\)\(+=._-]+$/g;//https://stackoverflow.com/questions/18812317/javascript-regex-for-special-characters
     //If there is a sign from regex above return massage about invalid name
-    if(name.match(specialSignReg) || name.match(numbersReg)){
+    if(name.match(specialSignOrNumbersReg)){
         return false;
     }else{
         return true;
